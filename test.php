d6806d713efbf67092dc58abf7e3abe419541790
DanielSimair <s1610238076@students.fh-hagenberg.at>
2016-12-13 14:51:35 +0100

    G irsi dropping sick lines, rhyming sick rhymes

--- test.php
+++ test.php
@@ -5,4 +5,5 @@
  * Date: 13.12.2016
  * Time: 13:46
  */
-echo '<p>Hallo Welt</p>'; ?>
+echo '<p>Hallo Welt</p>';
+echo '<p>brrrrrrrrt: G irsi in da house, und er geht dann nacher raus! sheesh</p>';
